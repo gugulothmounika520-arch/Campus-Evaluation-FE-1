fed366fc6d0fae032ecffda902aa84567b8a45f9
gugulothmounika520-arch <gugulothmounika520@gmail.com>
2026-06-18 13:39:18 +0530

    Update notifications.js

--- notification-app-fe/src/api/notifications.js
+++ notification-app-fe/src/api/notifications.js
@@ -1,2 +1,14 @@
-export function fetchNotifications() {
+export async function fetchNotifications(){
+  try{
+    const response = await fetch(
+      "http://4.224.186.213/evaluation-service/notifications");
+    if (!response.ok){
+      throw new Error ("Failed to fetch notifications");
+    }
+    const data = await response.json();
+    return data;
+  }
+  console.error("Error fetching notifications;", error);
+  retun[];
+}
 }
